587f7a8458f1d72175d1112e6393bd5cc32e2dd9
saadmughal0987 <saadnadeem504@gmail.com>
2025-08-31 19:11:39 +0500

    Updated navbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/saadmughal0987"
+                href="https://github.com/saadmughal0987/Saad-Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
